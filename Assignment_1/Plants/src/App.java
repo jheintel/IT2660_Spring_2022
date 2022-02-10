3b94fa8d0e3efae3cad0c49c6ef83c5bb2241958
Jonathan Heintel <darthpeach@gmail.com>
2022-02-10 13:35:52 -0500

    added comment

--- Assignment_1/Plants/src/App.java
+++ Assignment_1/Plants/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        
+        // I make a moss named Fred using an abstract class and an interface.
         Moss test = new Moss("Fred");
 
         System.out.println(test.getName());
