7c95aec055b228c856c42c8256db3c12b64f4063
Jonathan Heintel <darthpeach@gmail.com>
2022-04-29 19:08:11 -0400

    finished testing

--- Assignment_2/App.java
+++ Assignment_2/App.java
@@ -1,26 +1,50 @@
-import Plants.Moss;
+import Plants.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        // I make a moss named Fred using an abstract class and an interface.
-        Moss test = new Moss("0");
+        Moss test = new Moss("0 core moss");
 
+        //test adding past the base array size
         RollingStone roller = new RollingStone(test);
-        roller.addGrowth(new Moss("1"));
-        roller.addGrowth(new Moss("2"));
-        roller.addGrowth(new Moss("3"));
-        roller.addGrowth(new Moss("4"));
-        roller.addGrowth(new Moss("5"));
+        roller.addGrowth(new Moss("1 moss"));
+        roller.addGrowth(new Moss("2 moss"));
+        roller.addGrowth(new Moss("3 moss"));
+        roller.addGrowth(new Moss("4 moss"));
+        roller.addGrowth(new Moss("5 moss"));
 
-        roller.deleteGrowth("3");
-        roller.update(new Moss("insert"), 3);
+        //test deleting
+        roller.deleteGrowth("3 moss");
+        
+        //test updating past the base array size
+        roller.update(new Moss("insert1"), 3);
+        roller.update(new Moss("insert2"), 3);
+        roller.update(new Moss("insert3"), 3);
+        roller.update(new Moss("insert4"), 3);
+        roller.update(new Moss("insert5"), 3);
+        roller.update(new Moss("insert6"), 3);
+        roller.update(new Moss("insert7"), 3);
 
+        //add Mr Moss to help me make sure the tail end of the Array is being reached
         roller.addGrowth(new Moss("Mr. Moss"));
 
+        //delete past the reduce base array size threshhold
+        roller.deleteGrowth("insert2");
+        roller.deleteGrowth("insert3");
+        roller.deleteGrowth("insert4");
+        roller.deleteGrowth("insert5");
+        roller.deleteGrowth("insert6");
+        roller.deleteGrowth("insert7");
+
         roller.print();
-        //System.out.print("index check: ");
-        //System.out.println(roller.findIndex("insert"));
+        System.out.print("index check for \'Mr. Moss\': ");
+        System.out.println(roller.findIndex("Mr. Moss"));
+        System.out.print("The current dynamic array size is: ");
+        System.out.println(roller.getSize());
+        System.out.print("The current base array index size is: ");
+        System.out.println(roller.getTestIndex());
+
+
 
         //pressAnyKeyToContinue();
     }
--- Assignment_2/RollingStone.java
+++ Assignment_2/RollingStone.java
@@ -1,15 +1,19 @@
-
 import Plants.*;
+
+//This class should be treated as a dynamic Array of Moss objects
+//the theme of this stupid class was fun at first but now I'm cold on the inside
 public class RollingStone {
     private int next;
     private int size;
     private Moss[] stoneFace;
 
+    //basic constructor with no input
     public RollingStone(){
         this.next = 0;
         this.size = 5;
         this.stoneFace = new Moss[this.size];
     }
+    //constructor with an input Moss
     public RollingStone(Moss growth){
         this.next = 1;
         this.size = 5;
@@ -17,13 +21,26 @@ public class RollingStone {
         this.stoneFace[0] = growth;
     }
 
+    //return the Dynamic size of this array
+    public int getSize(){
+        return this.next;
+    }
+
+    //test function to return the actual size of the array object
+    public int getTestIndex(){
+        return stoneFace.length;
+    }
+
+    //print everything stored in the rollingStone
     public void print(){
-        for(int i=0; i+2<this.next; i++){
+        for(int i=0; i<this.next; i++){
             System.out.println(stoneFace[i].getName());
         }
     }
 
+    //method to add a new growth to the end of the rollingStone
     public void addGrowth(Moss newGrowth){
+        //if the length of the base array is reached make a larger one and store the old within the new
         if(this.next == this.size){
             int i =0;
             this.size+=5;
@@ -34,43 +51,90 @@ public class RollingStone {
             }
             this.stoneFace = biggerStone;
         }
-        this.stoneFace[next] = newGrowth;
+        this.stoneFace[this.next] = newGrowth;
         this.next++;
     }
 
-    public Moss fetchGrowth(String target){
-        return stoneFace[this.findIndex(target)];
+    //return the Moss at a certain index
+    public Moss fetchGrowth(int target){
+        return stoneFace[target];
     }
 
+    // return the index of a requested Moss
     public int findIndex(String target){
-        for(int i=0; i<this.size; i++){
+        for(int i=0; i<this.next; i++){
             if(this.stoneFace[i].getName().compareTo(target) == 0)
                 return i;
         }
+        //throw out a -1 of the Moss isn't found. Be a lot better to put an error handler for this
         return -1;
     }
 
+    //delete a growth by name
     public void deleteGrowth(String toDelete){
         int i = 0;
         i=findIndex(toDelete);
 
-        while(i<(next)){
+        //cycle through replacing lower indexes with what was above them after the delete point
+        while(i<this.next && i<this.size-1){
             stoneFace[i] = stoneFace[i+1];
             i++;
         }
         this.next--;
+
+        //if our dynamic array becomes 5 smaller than our base array reduce the size of the base array
+        if(this.next <= this.size-5){
+            int j =0;
+            this.size-=5;
+            Moss[] smallerStone= new Moss[this.size];
+            while(j<this.next){
+                smallerStone[j] = this.stoneFace[j];
+                j++;
+            }
+            this.stoneFace = smallerStone;
+        }
     }
 
+    //delete a growth by its index within the array
+    public void deleteGrowth(int toDelete){
+        int i = 0;
+        i=toDelete;
+
+        //cycle through replacing lower indexes with what was above them after the delete point
+        while(i<this.next && i<this.size-1){
+            stoneFace[i] = stoneFace[i+1];
+            i++;
+        }
+        this.next--;
+
+        //if our dynamic array becomes 5 smaller than our base array reduce the size of the base array
+        if(this.next <= this.size-5){
+            int j =0;
+            this.size-=5;
+            Moss[] smallerStone= new Moss[this.size];
+            while(j<this.next){
+                smallerStone[j] = this.stoneFace[j];
+                j++;
+            }
+            this.stoneFace = smallerStone;
+        }
+    }
+
+    //add a new moss in at a target index
+    //might also be a fun idea to create a method to add a Moss after another specified Moss
     public void update(Moss newMoss, int targetIndex){
         int i = 0;
         Moss temp;
-        while(i<this.next){
-            if(i>=targetIndex && this.next<=this.size){
+        while(i<=this.next){
+            if(i>=targetIndex && i!=this.size){
                 temp = this.stoneFace[i];
                 stoneFace[i] = newMoss;
                 newMoss = temp;
+            } else if (i==this.size){
+                this.addGrowth(newMoss);
+                //if I don't reduce the next count here the previous IF statement runs again
+                this.next--;
             }
-            
             i++;
         }
         this.next++;
